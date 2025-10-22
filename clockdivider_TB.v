ca8e5a2f8143e00a5e805e58081a587f44b10161
Frank Tamburro <fjt227@lehigh.edu>
2025-10-22 19:07:21 -0400

    Create clockdivider_TB.v

---
+++ clockdivider_TB.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/22/2025 04:13:36 PM
+// Design Name: 
+// Module Name: clockdivider_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clockdivider_TB();
+reg clock_in;
+wire clock_out;
+
+clockdivider DUT(.clock_in(clock_in),.clock_out(clock_out));
+initial 
+begin
+
+    clock_in = 0;
+
+    forever #10 clock_in=~clock_in;
+    
+end
+endmodule
